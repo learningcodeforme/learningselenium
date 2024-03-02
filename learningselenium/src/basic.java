73f4c7976df073e15f645ab544e3781691f44210
Nitesh Kumar <nitesh.hope@gmail.com>
2024-03-02 16:36:58 +0530

    learingcode

--- learningselenium/src/basic.java
+++ learningselenium/src/basic.java
@@ -23,15 +23,72 @@ public class basic {
 		// it will close all assoicate window
 		// driver.quit();
 		// locator learning
-		
+
 		driver.findElement(By.id("inputUsername")).sendKeys(" rahulshettyacademy420");
+
 		driver.findElement(By.cssSelector("input[type='password']")).sendKeys("1233434343535345");
+
 		driver.findElement(By.className("signInBtn")).click();
-		System.out.println("Alert text -- > "+ driver.findElement(By.cssSelector("p.error")).getText());
+
+		System.out.println("Alert text -- > " + driver.findElement(By.cssSelector("p.error")).getText());
+
 		driver.findElement(By.linkText("Forgot your password?")).click();
-		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("amazing");
-		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("amazing");
-		driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("12131313313");
+
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("enter by xpath");
+
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("enter by css");
+
+		// Phone number
+		// parent child traversing using xpath
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("parent child xpath");
+
+		// click on reset button
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+
+		// grab the text messasge using parent child concept using css
+		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+
+		// going back to login page using css
+
+
+		
+		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+		
+		Thread.sleep(2000);
+
+		// enter username using regular express in css
+	
+		driver.findElement(By.cssSelector("input[placeholder*='User']")).sendKeys("Nitesh");
+		
+		driver.findElement(By.cssSelector("input[placeholder*='Pass']")).sendKeys("rahulshettyacademy");
+		Thread.sleep(2000);
+		
+		//selecting checkbox 
+		driver.findElement(By.id("chkboxOne")).click();
+		
+		Thread.sleep(1000);
+		
+		// using regular express in xpath click sign button
+		
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+		
+		Thread.sleep(5000);
+		
+		driver.quit();
+		
+
+		
+		
+		
+		
+		
+
+		// xpath with regular expression
+
+		// css with regular expression
+
+		// parent child travering using css
+
 		// selenium support following locator
 		// id
 		// name
@@ -70,40 +127,59 @@ public class basic {
 		// wait in selenium
 		// implict and explict wait
 		// implict wait apply on every element in the webpage
-		 // we can verify the css what we written using selectorhub it is external plugin 
-		 // we can verify using developer tool console 
-		 // example - > $('css of locator')
-		 //$('p.error')
-		 //
-		 
-		 // Link Text 
-		 // a - > anchor tag
-		 //1. driver.findElement(By.linkText("text of link that appear"))
-		 
-		 // xpath
-		 // //tagname[@attibute='value']
-		
-		//css
-		 //  tagname[attribute="value"]
-		
-		// we can verify the xpath using developer tool console using $x('xpath')
-		 //$x('//input[@placeholder="Phone Number"]')
-		
-		
-		// index in xpath and css 
-		
-		//xpath using index in form of array
-		//driver.findElement(By.xpath("//input[@type='Text']"))[1]
-		
-	//	in css it uses :nth-child(3)
-		//driver.findElement(By.css("input[type='Text']")):nth-child(3) 
-		
-		
-		 
-		 
-		 
-		 
+		// we can verify the css what we written using selectorhub it is external plugin
+		// we can verify using developer tool console
+		// example - > $('css of locator')
+		// $('p.error')
+		//
 
+		// Link Text
+		// a - > anchor tag
+		// 1. driver.findElement(By.linkText("text of link that appear"))
+
+		// xpath
+		// //tagname[@attibute='value']
+
+		// css
+		// tagname[attribute="value"]
+
+		// we can verify the xpath using developer tool console using $x('xpath')
+		// $x('//input[@placeholder="Phone Number"]')
+
+		// index in xpath and css
+
+		// xpath using index in form of array
+		// driver.findElement(By.xpath("//input[@type='Text']"))[1]
+
+		// Example --> //input[@type='text'][3]
+
+		// in css it uses :nth-child(3)
+		// driver.findElement(By.css("input[type='Text']")):nth-child(3)
+
+		// example--> input[type='text']:nth-child(4)
+
+		// another way to write css and xpath
+		// Parent child traversing parent to child
+
+		// examples-- > //form/input[3]
+		// //div[@class='forgot-pwd-btn-conainer']/button[1] --> here we user parent full xpath and child we use tagname
+		// only with index
+		/*
+		 * //parenttagename/childtagename( syntax) use double // for before parent and
+		 * single / before child we can provide index if multiple child are available
+		 * 
+		 * 
+		 * in case of css
+		 * 
+		 * syntax - patent ( space) child form p where --> form is parent and p is child
+		 */
+
+		// Regular expression in css 
+		// Example -> "input[placeholder*='User']"   using asterik after arrtibute name
+		
+		// Regular expression in xpath
+		//Example -->    button[contains(@class,'submit')]  --> using contians key words 
+		
 	}
 
 }
