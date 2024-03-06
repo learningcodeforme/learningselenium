fc25d53f2b2d9619729a0088350b38eeed475407
Nitesh Kumar <nitesh.hope@gmail.com>
2024-03-06 21:43:44 +0530

    Assigment2 code without claender date

---
+++ learningselenium/src/Assignment2.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Assignment2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		driver.findElement(By.xpath("(//div/input)[1]")).sendKeys("Vinayak");
+		driver.findElement(By.cssSelector("input[name='email']")).sendKeys("vinayak@outlook.com");
+		driver.findElement(By.id("exampleInputPassword1")).sendKeys("*********");
+		driver.findElement(By.cssSelector("input[type='checkbox']")).click();
+		driver.findElement(By.cssSelector("#exampleFormControlSelect1")).click();
+		driver.findElement(By.xpath("//*[contains(text(),'Female')]")).click();
+		driver.findElement(By.xpath("//label[@for='inlineRadio2']")).click();
+		driver.findElement(By.xpath("//input[@name='bday']")).click();
+		driver.findElement(By.cssSelector("input[value='Submit")).click();
+		String msg = driver.findElement(By.cssSelector(".alert.alert-success.alert-dismissible")).getText();
+		System.out.println(" Message on page- " + msg);
+		driver.quit();
+
+	}
+
+}
